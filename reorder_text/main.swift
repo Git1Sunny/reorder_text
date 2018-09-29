043bdddbad2f85dee0bd92113ecdeba216a0fb30
Git1Sunny <764152162@qq.com>
2018-09-29 09:37:15 +0800

    reorder_number_text1
    Reorde number form 1~9

--- reorder_text/main.swift
+++ reorder_text/main.swift
@@ -8,5 +8,23 @@
 
 import Foundation
 
-print("Hello, World!")
+var myNumber : [Int] = [3,6,4,7,8,9,1,2,5,0]
 
+for count1 in 0...8
+{
+    for count2 in 0...(8 - count1)
+    {
+        if(myNumber[count2] < myNumber[count2 + 1])
+        {
+            var Temp : Int = 0
+            Temp = myNumber[count2]
+            myNumber[count2]=myNumber[count2 + 1]
+            myNumber[count2 + 1]=Temp
+        }
+    }
+}
+
+for count3 in myNumber
+{
+    print(myNumber[count3])
+}
